ff50c2d85f3444b2b2c7c17c7fc1729207c426b3
Moaz Osama <moazosama204@gmail.com>
2026-06-30 08:49:53 -0700

    add vendors item , list , section

---
+++ Drape/Home/Views/VendorSectionView.swift
@@ -0,0 +1,36 @@
+//
+//  VendorSectionView.swift
+//  Drape
+//
+//  Created by Moaz on 30/06/2026.
+//
+
+import SwiftUI
+
+struct VendorSectionView: View {
+    var onTap: () -> Void = {}
+    var body: some View {
+        VStack(alignment: .leading,spacing: 16.0) {
+            HStack() {
+                Text("Our Vendors")
+                    .font(.system(size: 18.0,weight: .semibold))
+                    .foregroundStyle(Color(hex: "121926"))
+                
+                Spacer()
+                
+                Button(action: onTap) {
+                    Text("See All")
+                        .font(.system(size: 16.0,weight: .regular))
+                        .foregroundStyle(Color(hex: "B3B3B3"))
+                }
+                
+            }
+            
+            VendorItemsList()
+        }
+    }
+}
+
+#Preview {
+    VendorSectionView()
+}
