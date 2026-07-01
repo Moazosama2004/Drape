f049b0eed4b3f13e2baee8f0b5368290a62e1908
Youssef Abd El-Fatah <99736061+YoussefAbdel-Fatah@users.noreply.github.com>
2026-07-01 12:48:41 -0700

    Reorganize Home module with Clean Architecture
    Restructure the Home module to separate concerns using Clean Architecture principles. Move Views and Components into a Presentation layer and introduce a Data layer with DTOs. Add ProductDTO to model product data with variants and images for API responses.

---
+++ Drape/Home/Data/DTOs/ProductDTO.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  Drape
+//
+//  Created by Youssef Abd El-Fatah on 01/07/2026.
+//
+
+import Foundation
+
+struct ProductDTO: Codable {
+    
+    let id: Int
+    let title: String
+    let vendor: String // ex: Adidas
+    let productType: String // ex: Shoes
+    let variants: [VariantDTO] // gets the price
+    let image: ImageDTO // gets the image link
+}
+
+struct VariantDTO: Codable {
+    let price: String
+}
+
+struct ImageDTO: Codable {
+    let src: String
+}
